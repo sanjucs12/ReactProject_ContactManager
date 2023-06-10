493d11a6906f97fded730f11f3e318931ff2ad5c
sanjucs12 <sanjucs12@gmail.com>
2023-06-10 21:38:00 +0530

    Folder structure and required components added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-      </header>
-    </div>
+    <div>Hello world</div>
   );
 }
 
